04fcd8a36eaf96dc80852ea09020210a31c67f50
KongBerg <nikolaj.kongsgaard.berg@gmail.com>
2023-12-08 12:37:07 +0100

    VI prøver på admin lort

--- Vagtplan/Shared/Model/Vagter.cs
+++ Vagtplan/Shared/Model/Vagter.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace Musikfestival.Shared.Models
 {
@@ -18,6 +19,8 @@ namespace Musikfestival.Shared.Models
         public string Slut { get; set; } = String.Empty;
         public string Username { get; set; } = String.Empty;
         public string Beskrivelse { get; set; } = String.Empty;
+
+        [JsonIgnore] public bool IsEditing { get; set;}
     }
 }
 
