9e7affb440945cfae8a8dea44684249364c0a35b
KongBerg <nikolaj.kongsgaard.berg@gmail.com>
2023-12-08 09:23:32 +0100

    Dato fikset

--- Vagtplan/Shared/Model/Vagter.cs
+++ Vagtplan/Shared/Model/Vagter.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel;
 
 namespace Musikfestival.Shared.Models
 {
@@ -8,12 +9,13 @@ namespace Musikfestival.Shared.Models
         [BsonId]
         [BsonRepresentationAttribute(BsonType.ObjectId)]
         public ObjectId Id { get; set; }
+        public string Dato { get; set; } = String.Empty;
         public string Lokation { get; set; } = String.Empty;
         public int Rangering { get; set; } = 0;
         public string Type { get; set; } = String.Empty;
         public int Antal { get; set; } = 0;
-        public DateTime Start { get; set; } = DateTime.Now;
-        public DateTime Slut { get; set; } = DateTime.Now;
+        public string Start { get; set; } = String.Empty;
+        public string Slut { get; set; } = String.Empty;
         public string Beskrivelse { get; set; } = String.Empty;
     }
 }
